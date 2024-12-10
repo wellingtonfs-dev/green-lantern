c094cad15eba5ffd051d47d6c1cc9183023e6000
Wellington Ferreira da Silva <wellingtonfs50@gmail.com>
2024-12-09 22:04:58 -0300

    Projeto concluido

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, TouchableOpacity, View, Image } from 'react-native';
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
 
-export default function App() {
+export default function App() {  
+  const [isActive, setIsActive] = useState(false);
+
+  function handleSymbol(){
+    setIsActive((oldValue:boolean) => {
+      return !oldValue;
+    });    
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSymbol}>
+      <Image source={isActive ? symbolOn : symbolOff}/>
+
+      </TouchableOpacity>
+          
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerOff: {
+    flex: 1,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
